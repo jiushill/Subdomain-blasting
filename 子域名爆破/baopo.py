5e6603f364e23d7b725ddbe71debb3b42bd1458a
九世 <422926799@qq.com>
2019-02-03 21:05:40 +0800

    Add files via upload

---
+++ 子域名爆破/baopo.py
@@ -0,0 +1,199 @@
+#author:九世
+#time:2019/2/1
+
+import requests
+import threading
+import os
+import time
+import socket
+import re
+
+dict=[]
+found_domain=[]
+lock=threading.BoundedSemaphore(100)
+
+class Rkst:
+    def __init__(self,headers):
+        self.headers=headers
+
+    def jiekou(self,url,data):
+        try:
+            rqt=requests.post(url=url,headers=self.headers,data=data)
+            jso=rqt.json()['data']
+            if len(jso)>0:
+                for r in jso:
+                    ip=self.sok(r['domain'])
+                    title=self.title(r['domain'])
+                    print('[+] 查询到的域名：{} IP地址：{} {}'.format(r['domain'],ip,title))
+                    print('[+] 查询到的域名：{} IP地址：{} {}'.format(r['domain'],ip,title),file=open('save.txt','a'))
+            else:
+                print('[-] 无数据')
+        except Exception as r:
+            print('[-] 神奇的报错冒了出来：{}'.format(r))
+
+        lock.release()
+    def shenc(self,file):
+            for k in file.readlines():
+                qc="".join(k.split('\n'))
+                yield qc
+
+    def one_domain(self,url):
+        for q in dict:
+            urls=q+'.'+url
+            ip=self.sok(urls)
+            if ip:
+                title=self.title(urls)
+                print('[+] 爆破到的域名：{} IP地址：{} {}'.format(urls,ip,title))
+
+        lock.release()
+    def two_domain(self,url):
+        for v in dict:
+            for v2 in dict:
+                urls=v+'.'+v2+'.'+url
+                ip = self.sok(urls)
+                if ip:
+                    title = self.title(urls)
+                    print('[+] 爆破到的域名：{} IP地址：{} {}'.format(urls, ip, title))
+
+        lock.release()
+    def san_domain(self,url):
+        for u in dict:
+            for u1 in dict:
+                for u2 in dict:
+                    urls=u+'.'+u1+'.'+u2+'.'+url
+                    ip = self.sok(urls)
+                    if ip:
+                        title = self.title(urls)
+                        print('[+] 爆破到的域名：{} IP地址：{} {}'.format(urls, ip, title))
+
+        lock.release()
+    def si_domain(self,url):
+        for s in dict:
+            for s1 in dict:
+                for s2 in dict:
+                    for s3 in dict:
+                        urls=s+'.'+s1+'.'+s2+'.'+s3+'.'+url
+                        ip = self.sok(urls)
+                        if ip:
+                            title = self.title(urls)
+                            print('[+] 爆破到的域名：{} IP地址：{} {}'.format(urls, ip, title))
+
+        lock.release()
+    def wu_domain(self,url):
+        for b in dict:
+            for b1 in dict:
+                for b2 in dict:
+                    for b3 in dict:
+                        for b4 in dict:
+                            urls=b+'.'+b1+'.'+b2+'.'+b3+'.'+b4+'.'+url
+                            ip = self.sok(urls)
+                            if ip:
+                                title = self.title(urls)
+                                print('[+] 爆破到的域名：{} IP地址：{} {}'.format(urls, ip, title))
+        lock.release()
+    def sok(self,domain):
+        try:
+            s=socket.getaddrinfo(domain,None)
+            return str(s[0][4]).replace("'",'').replace('0','').replace('(','').replace(')','').replace(",",'')
+        except:
+            pass
+
+    def title(self,domain):
+        try:
+            url='http://{}'.format(domain)
+            url2='https://{}'.format(domain)
+            rqt=requests.get(url=url,headers=self.headers,timeout=1)
+            if rqt:
+                zz=re.findall('<title>.*</title>',rqt.content.decode('utf-8'))
+                if 'Server' in rqt.headers:
+                    r=rqt.headers
+                    server=r['Server']
+                else:
+                    server=None
+                return '标题：{} web环境：{}'.format(str(zz[0]).replace('<title>','').replace('</title>',''),server)
+            else:
+                rq2=requests.get(url=url2,headers=self.headers,timeout=1)
+                zz = re.findall('<title>.*</title>', rq2.content.decode('utf-8'))
+                if 'Server' in rqt.headers:
+                    r=rqt.headers
+                    server=r['Server']
+                else:
+                    server=None
+                return '标题：{} web环境：{}'.format(str(zz[0]).replace('<title>', '').replace('</title>', ''),server)
+        except:
+            return None
+            pass
+
+if __name__ == '__main__':
+    headers={'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    jkl='http://sbd.ximcx.cn/DomainServlet'
+    print('九世版子域名查询_何安圻')
+    user=input('查询的域名：')
+    print('[@] 接下来设置爆破模式，输入1为爆破一级域名，输入2为爆破二级域名，输入3位爆破三级域名，输入4为爆破四级域名，输入5位爆破5级域名，输入all为全开 注意：此模式很慢')
+    xw=input('设置爆破模式：')
+    data={'domain': '{}'.format(user)}
+    obj=Rkst(headers=headers)
+
+    print('[*] 接口查询模式')
+    lock.acquire()
+    t=threading.Thread(target=obj.jiekou,args=(jkl,data))
+    t.start()
+
+
+    if os.path.exists('file/one.txt'):
+        print('[@] 找到了神奇的爆破字典')
+    else:
+        print('[-] 找不到字典= =#')
+        print('[-] 退出程序...')
+        exit()
+
+    dk=open('file/one.txt','r')
+    for r in obj.shenc(dk):
+        dict.append(r)
+
+    print('[*] 爆破模式')
+    if xw=='1':
+        lock.acquire()
+        s=threading.Thread(target=obj.one_domain,args=(user,))
+        s.start()
+
+    elif xw=='2':
+        lock.acquire()
+        s2 = threading.Thread(target=obj.two_domain, args=(user,))
+        s2.start()
+
+    elif xw=='3':
+        lock.acquire()
+        s3 = threading.Thread(target=obj.san_domain, args=(user,))
+        s3.start()
+
+    elif xw=='4':
+        lock.acquire()
+        s4 = threading.Thread(target=obj.si_domain, args=(user,))
+        s4.start()
+
+    elif xw=='5':
+        lock.acquire()
+        s5 = threading.Thread(target=obj.wu_domain, args=(user,))
+        s5.start()
+
+    elif xw=='all':
+        lock.acquire()
+        s = threading.Thread(target=obj.one_domain, args=(user,))
+        s.start()
+
+        lock.acquire()
+        s2 = threading.Thread(target=obj.two_domain, args=(user,))
+        s2.start()
+
+        lock.acquire()
+        s3 = threading.Thread(target=obj.san_domain, args=(user,))
+        s3.start()
+
+        lock.acquire()
+        s4 = threading.Thread(target=obj.si_domain, args=(user,))
+        s4.start()
+
+        lock.acquire()
+        s5 = threading.Thread(target=obj.wu_domain, args=(user,))
+        s5.start()
